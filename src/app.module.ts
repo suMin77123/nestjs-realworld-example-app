0eac0a00309beccfe44586bbfc1ecd4660b69983
suMin <sumin@pia.space>
2025-08-20 13:33:05 +0900

    feat: 필요한 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ArticlesModule } from './articles/articles.module';
+import { CommentsModule } from './comments/comments.module';
+import { ProfilesModule } from './profiles/profiles.module';
+import { TagsModule } from './tags/tags.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, ArticlesModule, CommentsModule, ProfilesModule, TagsModule, SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
