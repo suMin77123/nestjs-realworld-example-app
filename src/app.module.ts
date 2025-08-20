d23e569ca20c36285435f4e6d0ddc0488eae4b8e
suMin <sumin@pia.space>
2025-08-20 13:49:03 +0900

    chore: app 모듈 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,7 +11,19 @@ import { TagsModule } from './tags/tags.module';
 import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, ArticlesModule, CommentsModule, ProfilesModule, TagsModule, SharedModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    UsersModule,
+    AuthModule,
+    ArticlesModule,
+    CommentsModule,
+    ProfilesModule,
+    TagsModule,
+    SharedModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
