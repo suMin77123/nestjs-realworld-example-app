1109eef6d580472ad986ca27e1ec778f25b44e7b
suMin <sumin77123@gmail.com>
2025-09-16 00:09:09 +0900

    feat: profile GET 기능 추가

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ProfilesService } from './profiles.service';
+import { ProfileResponseDto } from './dto/profile-response.dto';
 
 @Controller('profiles')
-export class ProfilesController {}
+export class ProfilesController {
+  constructor(private readonly profilesService: ProfilesService) {}
+
+  @Get(':username')
+  @UseGuards(JwtAuthGuard)
+  async getProfile(@Param('username') username: string): Promise<ProfileResponseDto> {
+    return this.profilesService.getProfile(username);
+  }
+}
--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Profile } from './profile.entity';
+import { Repository } from 'typeorm';
+import { ProfileResponseDto } from './dto/profile-response.dto';
 
 @Injectable()
-export class ProfilesService {}
+export class ProfilesService {
+  constructor(
+    @InjectRepository(Profile)
+    private profilesRepository: Repository<Profile>,
+  ) {}
+
+  async getProfile(username: string): Promise<ProfileResponseDto> {
+    const profile = await this.profilesRepository.findOne({ where: { username } });
+
+    if (!profile) {
+      throw new NotFoundException('Profile not found');
+    }
+
+    return profile;
+  }
+}
